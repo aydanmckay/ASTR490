36b8b3ea7bbe85e908dd44c538d4fbf678c7a22f
aydanmckay <51837264+aydanmckay@users.noreply.github.com>
2021-07-02 15:52:54 -0700

    Add files via upload
    1st time adding the files

---
+++ ml/configcreator.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  2 12:06:45 2021
+
+@author: aydan
+"""
+
+from configparser import ConfigParser
+
+#Get the configparser object
+config_object = ConfigParser()
+
+# create (so far) 4 sections of the config file such that I only need to
+# change the section being being used to change how the program operates.
+config_object['baseparams'] = {
+    'db': 'D:/dataverse_files/v2/hii_v2_20201203.db',
+    'outputdir': 'D:/ASTR490/Catalog/',
+    'imsize': '1',
+    'Allsky': '0',
+    'glongmax': '305',
+    'glongmin': '301',
+    'glatmax': '1',
+    'glatmin': '-1',
+    'gname': 'None',
+    'glong': '361',
+    'glat': '0',
+    'regionsize': '0'
+}
+
+config_object['Allskyparams'] = {
+    'db': 'D:/dataverse_files/v2/hii_v2_20201203.db',
+    'outputdir': 'D:/ASTR490/Allskyfolder/',
+    'imsize': '1',
+    'Allsky': '1',
+    'glongmax': '0',
+    'glongmin': '0',
+    'glatmax': '0',
+    'glatmin': '0',
+    'gname': 'None',
+    'glong': '361',
+    'glat': '0',
+    'regionsize': '0'
+}
+
+config_object['knownregion'] = {
+    'db': 'D:/dataverse_files/v2/hii_v2_20201203.db',
+    'outputdir': 'D:/ASTR490/knownreg/',
+    'imsize': '0',
+    'Allsky': '1',
+    'glongmax': '0',
+    'glongmin': '0',
+    'glatmax': '0',
+    'glatmin': '0',
+    'gname': 'G297.651-00.973',
+    'glong': '361',
+    'glat': '0',
+    'regionsize': '0'
+}
+
+config_object['coords'] = {
+    'db': 'D:/dataverse_files/v2/hii_v2_20201203.db',
+    'outputdir': 'D:/ASTR490/randcoords/',
+    'imsize': '0',
+    'Allsky': '1',
+    'glongmax': '0',
+    'glongmin': '0',
+    'glatmax': '0',
+    'glatmin': '0',
+    'gname': 'None',
+    # l [###.###],b [+/-##.###]
+    'glong': '087.000', 
+    'glat': '+00.500',
+    'regionsize': '0.3'
+}
+
+#Write the above sections to config.ini file
+with open('C:/Users/aydan/OneDrive/Documents/UVic_2021/Astr 490/ml/config.ini', 'w') as conf:
+    config_object.write(conf)
---
+++ ml/displayregion.py
@@ -0,0 +1,447 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 16 17:05:00 2021
+
+@author: aydan
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import sqlite3
+import shutil
+import astropy.units as u
+from astroquery.skyview import SkyView
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+from astropy.wcs import WCS
+from astropy.coordinates import SkyCoord
+import time
+from configparser import ConfigParser
+
+def get_wise_catalog(db):
+    """
+    Return a pandas dataframe containing relevant data from
+    the WISE Catalog.
+
+    Inputs:
+        db :: string
+            Filename to the HII region database
+
+    Returns: data
+        data :: pandas.DataFrame
+            DataFrame containing the WISE Catalog data
+    """
+    with sqlite3.connect(db) as conn:
+        cur = conn.cursor()
+        cur.execute("PRAGMA foreign_keys = ON")
+        cur.execute("SELECT gname, ra, dec, radius FROM Catalog")
+        data = pd.DataFrame(
+            cur.fetchall(), columns=[d[0] for d in cur.description])
+    return data
+
+def get_wise_images(gname, ra, dec, size, outdir):
+    """
+    Return the WISE 3.4, 12, and 22 micron data for a given sky
+    position.
+
+    Inputs:
+        gname :: string
+            Source name. Images are saved to
+            f"{outdir}/{gname}_wise_3.4.fits", etc.
+        ra, dec :: scalars (deg)
+            Cental sky position (J2000)
+        size :: scalar (deg)
+            Image cutout size
+        outdir :: string
+            Directory where downloaded FITS images are saved
+
+    Returns: wise_3, wise_12, wise_22
+        wise_3, wise_12, wise_22 :: astropy.fits.HDU
+            The WISE 3.4, 12, and 22 micron FITS HDUs
+    """
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    # SkyView occasionally fails, so we attempt multiple downloads
+    success = False
+    count = 0
+    while not success:
+        if count > 0:
+            # Due to bug in astroquery.SkyView, a failed download is
+            # cached and the cache directory must be deleted before
+            # a new attempt is made
+            shutil.rmtree(SkyView.cache_location)
+            os.mkdir(SkyView.cache_location)
+        if count > 10:
+            raise ValueError(
+                f"Exceeded download attempt limit for {gname}")
+        try:
+            # WISE 3.4 micron
+            fname = os.path.join(outdir, f"{gname}_wise_3.4.fits")
+            images = SkyView.get_images(
+                position=f"{ra:.3f}, {dec:.3f}", coordinates="J2000",
+                pixels=1024, width=size*u.deg, height=size*u.deg, survey="WISE 3.4")
+            wise_3_hdu = images[0][0]
+            wise_3_hdu.writeto(fname, overwrite=True)
+
+            # WISE 12 micron
+            fname = os.path.join(outdir, f"{gname}_wise_12.fits")
+            images = SkyView.get_images(
+                position=f"{ra:.3f}, {dec:.3f}", coordinates="J2000",
+                pixels=1024, width=size*u.deg, height=size*u.deg, survey="WISE 12")
+            wise_12_hdu = images[0][0]
+            wise_12_hdu.writeto(fname, overwrite=True)
+
+            # WISE 22 micron
+            fname = os.path.join(outdir, f"{gname}_wise_22.fits")
+            images = SkyView.get_images(
+                position=f"{ra:.3f}, {dec:.3f}", coordinates="J2000",
+                pixels=1024, width=size*u.deg, height=size*u.deg, survey="WISE 22")
+            wise_22_hdu = images[0][0]
+            wise_22_hdu.writeto(fname, overwrite=True)
+
+            # success
+            success = True
+        except:
+            count += 1
+            time.sleep(1)
+            
+    return wise_3_hdu, wise_12_hdu, wise_22_hdu
+
+def getcoords(coords):
+    """
+    Convets user inputted galactic coordinates to RA and Dec.
+
+    Parameters
+    ----------
+    coords : list of strings
+        The coordinates in Galactic coordinates inputted by the user.
+
+    Returns
+    -------
+    newcoords : list of strings
+        The coordinates in RA and Dec.
+    """
+    
+    skycoordthing = SkyCoord(frame='galactic', l=coords[0],
+                             b=coords[1], unit='deg').icrs
+    newcoords = [skycoordthing.ra.deg,skycoordthing.dec.deg]
+    
+    return newcoords
+
+
+def scale(data, vmin, vmax):
+    """
+    Clip and logarithmically scale some data.
+
+    Inputs:
+        data :: ndarray of scalars
+            Data to clip and scale
+        vmin, vmax :: scalars
+            Minimum and maximum percentiles for clipping
+
+    Returns: newdata
+        newdata :: ndarray of scalars
+            Clipped and scaled data
+    """
+    # logarithimically scale
+    data = np.log10(data)
+
+    # percentile clip
+    cut = np.nanpercentile(data, vmin)
+    data[data < cut] = cut
+    cut = np.nanpercentile(data, vmax)
+    data[data > cut] = cut
+
+    # set minimum value to zero
+    data = data - np.nanmin(data)
+
+    # set maximum value to one
+    data = data / np.nanmax(data)
+    return data
+
+def knownreg(db, outfile, gname):
+    # Get the WISE Catalog data
+    wise_catalog = get_wise_catalog(db)
+
+    # Get WISE infrared data
+    row = wise_catalog.loc[wise_catalog["gname"] == gname]
+    if len(row) == 0:
+        raise ValueError(f"{gname} not found in WISE Catalog!")
+    row = row.iloc[0]
+    # image size is three times larger than source radius
+    imsize = 3.0 * row["radius"]/3600.0
+    wise_3, wise_12, wise_22 = get_wise_images(
+        gname, row["ra"], row["dec"], imsize, outfile)
+
+    # Clip and scale infrared data
+    image_r = scale(wise_22.data, 10.0, 99.0)
+    image_g = scale(wise_12.data, 10.0, 99.5)
+    image_b = scale(wise_3.data, 10.0, 99.5)
+    image = np.stack([image_r, image_g, image_b], axis=-1)
+
+    # Generate figure
+    fig = plt.figure()
+    wcs = WCS(wise_3.header).celestial
+    ax = plt.subplot(projection=wcs)
+    ax.imshow(image, origin="lower", interpolation="none")
+    ax.set_xlabel("RA (J2000)")
+    ax.set_ylabel("Declination (J2000)")
+    # get pixel position and radius of the WISE Catalog source
+    xpos, ypos = wcs.wcs_world2pix(row["ra"], row["dec"], 1)
+    radius = row["radius"] / 3600.0 / wise_3.header["CDELT2"]
+    circle = Circle(
+        (xpos, ypos), radius, fill=False,
+        linestyle="dashed", color="yellow")
+    ax.add_artist(circle)
+    fig.savefig(outfile+gname+'_wise.pdf', bbox_inches="tight")
+    plt.close(fig)
+    
+def auto_get_wise_images(gname, ra, dec, w, h, outdir):
+    """
+    Return the WISE 3.4, 12, and 22 micron data for a given sky
+    position. Automatic version.
+
+    Inputs:
+        gname :: string
+            Source name. Images are saved to
+            f"{outdir}/{gname}_wise_3.4.fits", etc.
+        ra, dec :: scalars (deg)
+            Cental sky position (J2000)
+        size :: scalar (deg)
+            Image cutout size
+        outdir :: string
+            Directory where downloaded FITS images are saved
+
+    Returns: wise_3, wise_12, wise_22
+        wise_3, wise_12, wise_22 :: astropy.fits.HDU
+            The WISE 3.4, 12, and 22 micron FITS HDUs
+    """
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    # SkyView occasionally fails, so we attempt multiple downloads
+    success = False
+    count = 0
+    while not success:
+        if count > 0:
+            # Due to bug in astroquery.SkyView, a failed download is
+            # cached and the cache directory must be deleted before
+            # a new attempt is made
+            shutil.rmtree(SkyView.cache_location)
+            os.mkdir(SkyView.cache_location)
+        if count > 10:
+            print(f"Exceeded download attempt limit for {gname}")
+            return 'fail','fail','fail'
+        try:
+            # WISE 3.4 micron
+            #  = osfname.path.join(outdir, f"{gname}_wise_3.4.fits")
+            images = SkyView.get_images(
+                position=f"{ra:.3f}, {dec:.3f}", coordinates="J2000",
+                pixels=1024, width=w*u.deg, height=h*u.deg, survey="WISE 3.4")
+            wise_3_hdu = images[0][0]
+            # wise_3_hdu.writeto(fname, overwrite=True)
+
+            # WISE 12 micron
+            # fname = os.path.join(outdir, f"{gname}_wise_12.fits")
+            images = SkyView.get_images(
+                position=f"{ra:.3f}, {dec:.3f}", coordinates="J2000",
+                pixels=1024, width=w*u.deg, height=h*u.deg, survey="WISE 12")
+            wise_12_hdu = images[0][0]
+            # wise_12_hdu.writeto(fname, overwrite=True)
+
+            # WISE 22 micron
+            # fname = os.path.join(outdir, f"{gname}_wise_22.fits")
+            images = SkyView.get_images(
+                position=f"{ra:.3f}, {dec:.3f}", coordinates="J2000",
+                pixels=1024, width=w*u.deg, height=h*u.deg, survey="WISE 22")
+            wise_22_hdu = images[0][0]
+            # wise_22_hdu.writeto(fname, overwrite=True)
+
+            # success
+            success = True
+        except:
+            count += 1
+            time.sleep(0.2)
+            
+    return wise_3_hdu, wise_12_hdu, wise_22_hdu
+
+
+# def main(db, outfile, gname = 'None', coords = [361,0], dims = [0,0], Allsky = True):
+def main(section):
+    """
+    Generate a WISE infrared three-color catalog containing WISE HII Regions.
+
+    Inputs:
+        section :: string
+            specfic section of the config file to be used, 
+            determining what catalog will be generated
+
+    Returns: Nothing
+    """
+    
+    #Read config.ini file
+    config_object = ConfigParser()
+    config_object_file = 'C:/Users/aydan/OneDrive/Documents/UVic_2021/Astr 490/ml/config.ini'
+    config_object.read(config_object_file)
+    config = config_object[section] #######################################
+    dims = [float(config['imsize']),float(config['imsize'])]
+    
+    if config['gname'] != 'None':
+        knownreg(config['db'], config['outputdir'], config['gname'])
+        
+    if dims[0] != 0:
+        if bool(int(config['Allsky'])) == True:
+            # startTime = time.time()
+            l_list = np.arange(0,360,dims[0])+dims[0]/2
+            b_list = np.arange(-90,90,dims[1])+dims[1]/2
+            ll,bb = np.meshgrid(l_list,b_list)
+            ls = ll.flatten()
+            bs = bb.flatten()
+            for it,(l,b) in enumerate(zip(ls,bs)):    
+                radec = getcoords([l,b])
+                gname = 'A'+str(l)+str(b)
+                wise_3, wise_12, wise_22 = auto_get_wise_images(
+                gname, radec[0], radec[1], dims[0], dims[1], config['outputdir'])
+                if wise_3 == 'fail':
+                    continue
+            
+                # Clip and scale infrared data
+                image_r = scale(wise_22.data, 10.0, 99.0)
+                image_g = scale(wise_12.data, 10.0, 99.5)
+                image_b = scale(wise_3.data, 10.0, 99.5)
+                image = np.stack([image_r, image_g, image_b], axis=-1)
+            
+            
+                # Generate figure
+                fig = plt.figure()
+                wcs = WCS(wise_3.header).celestial
+                ax = plt.subplot(projection=wcs)
+                ax.imshow(image, origin="lower", interpolation="none")
+                ax.set_xlabel("RA (J2000)")
+                ax.set_ylabel("Declination (J2000)")
+                fig.savefig(config['outputdir']+gname+'_wise.pdf', bbox_inches="tight")
+                plt.close(fig)
+                # executionTime = (time.time() - startTime)
+                # print('Execution time in seconds: ' + str(executionTime))
+                # break
+            
+            
+        if bool(int(config['Allsky'])) == False:
+            upperglong = float(config['glongmax'])
+            lowerglong = float(config['glongmin'])
+            upperglat = float(config['glatmax'])
+            lowerglat = float(config['glatmin'])
+            l_list = np.arange(lowerglong,upperglong,dims[0])+dims[0]/2
+            b_list = np.arange(lowerglat,upperglat,dims[1])+dims[1]/2
+            ll,bb = np.meshgrid(l_list,b_list)
+            ls = ll.flatten()
+            bs = bb.flatten()
+            for it,(l,b) in enumerate(zip(ls,bs)):  
+                # startTime = time.time()
+                radec = getcoords([l,b])
+                gname = 'A'+str(l)+str(b)
+                wise_3, wise_12, wise_22 = auto_get_wise_images(
+                gname, radec[0], radec[1], dims[0], dims[1], config['outputdir'])
+                if wise_3 == 'fail':
+                    continue
+            
+                # Clip and scale infrared data
+                image_r = scale(wise_22.data, 10.0, 99.0)
+                image_g = scale(wise_12.data, 10.0, 99.5)
+                image_b = scale(wise_3.data, 10.0, 99.5)
+                image = np.stack([image_r, image_g, image_b], axis=-1)
+            
+            
+                # Generate figure
+                fig = plt.figure()
+                wcs = WCS(wise_3.header).celestial
+                ax = plt.subplot(projection=wcs)
+                ax.imshow(image, origin="lower", interpolation="none")
+                ax.set_xlabel("RA (J2000)")
+                ax.set_ylabel("Declination (J2000)")
+                fig.savefig(config['outputdir']+gname+'_wise.pdf', bbox_inches="tight")
+                plt.close(fig)
+                # executionTime = (time.time() - startTime)
+                # print('Execution time in seconds: ' + str(executionTime))
+                # break
+        
+    if float(config['glong']) != 361:
+        
+        # Get the WISE Catalog data
+        wise_catalog = get_wise_catalog(config['db'])
+    
+        # Get WISE infrared data
+        row = wise_catalog.loc[wise_catalog["gname"] == 'G'+config['glong']+config['glat']]
+        if len(row) != 0:
+            print('Actually a known HII Region!')
+            gname = 'G'+config['glong']+config['glat']
+            knownreg(config['db'], config['outputdir'], gname)
+        
+        else:
+            gname = 'NG'+config['glong']+config['glat']
+            radec = getcoords([config['glong'],config['glat']])
+            
+            imsize = config['regionsize']
+            
+            wise_3, wise_12, wise_22 = get_wise_images(
+                gname, radec[0], radec[1], imsize, config['outputdir'])
+            
+            # Clip and scale infrared data
+            image_r = scale(wise_22.data, 10.0, 99.0)
+            image_g = scale(wise_12.data, 10.0, 99.5)
+            image_b = scale(wise_3.data, 10.0, 99.5)
+            image = np.stack([image_r, image_g, image_b], axis=-1)
+            
+            
+            # Generate figure
+            fig = plt.figure()
+            wcs = WCS(wise_3.header).celestial
+            ax = plt.subplot(projection=wcs)
+            ax.imshow(image, origin="lower", interpolation="none")
+            ax.set_xlabel("RA (J2000)")
+            ax.set_ylabel("Declination (J2000)")
+            fig.savefig(config['outputdir']+gname+'_wise.pdf', bbox_inches="tight")
+            plt.close(fig)
+            
+    
+# if __name__ == "__main__":
+#     main()
+    
+# =============================================================================
+#     gnamechecker = True
+#     while gnamechecker:
+#         batchcontrol = input('Automatically create image catalog? (y/n): ')
+#         if batchcontrol == 'y':
+#             print('Image Dimensions (For now choose factors of 360)')
+#             dim_l = float(input('...For Galactic Longitude?: '))
+#             dim_b = float(input('...For Galactic Latitude?: '))
+#             dims = [dim_l,dim_b]
+#             skycoverage = input('All sky? (Type \"y\" for total sky coverage): ')
+#             allsky = True
+#             if skycoverage != 'y':
+#                 allsky = False   
+#             main("D:/dataverse_files/v2/hii_v2_20201203.db",
+#                   "D:/ASTR490/",
+#                   dims = dims, Allsky=allsky)
+#             break
+#         elif batchcontrol != 'n':
+#             print('Invalid input...')
+#             continue
+#         boolregion = input('Known HII Region? (y/n): ')
+#         if boolregion == 'y':
+#             gname = input('Name of the region: ')
+#             main("D:/dataverse_files/v2/hii_v2_20201203.db",
+#                   "C:/Users/aydan/OneDrive/Documents/UVic_2021/Astr 490/",
+#                   gname = gname)
+#             gnamechecker = False
+#         elif boolregion == 'n':
+#             coordsstring = input('Galactic Coordinates? (l [###.###],b [+/-##.###]): ')
+#             main("D:/dataverse_files/v2/hii_v2_20201203.db",
+#                   "C:/Users/aydan/OneDrive/Documents/UVic_2021/Astr 490/",
+#                   coords = coordsstring.split(','))
+#             gnamechecker = False
+#         else:
+#             print('Invalid input...')
+# =============================================================================
